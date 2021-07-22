b972f202c7a9c36b7a2e1fa40e00cc0967e875a0
Isael <isaelnobre@gmail.com>
2021-07-22 19:20:02 -0300

    quinto commit

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoTeste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello  God");
+            Console.WriteLine("Seeeeebastiao");
         }
     }
 }
